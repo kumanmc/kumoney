f43cb3d2b80810bc593df36092ceb3a054dbe56d
kumanmc <mdavid29021984@gmail.com>
2023-12-07 12:13:52 +0100

    Default value for current field

--- backend/api/models.py
+++ backend/api/models.py
@@ -3,6 +3,7 @@ from django.db import models
 class Bank(models.Model):
     symbol = models.CharField(max_length=20)
     goal = models.FloatField()
+    current = models.FloatField(default=0.0)
 
     def __str__(self):
         return f"{self.symbol} - Goal: {self.goal}"
