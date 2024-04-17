c93bbc1dd89e07b4e9158d11bd885f5ca8915c56
kumanmc <mdavid29021984@gmail.com>
2024-04-17 16:39:20 +0200

    Comment binance to hide this service in the api

--- backend/api/urls.py
+++ backend/api/urls.py
@@ -6,6 +6,6 @@ from .kumoney.views import GetBankView
 # path(endPoint url, View object, method name in service)
 
 urlpatterns = [
-    path('account/', AccountInfoView.as_view(), name='account_info'),
+    # path('account/', AccountInfoView.as_view(), name='account_info'),
     path('bank/', GetBankView.as_view(), name='get_bank'),
 ]
